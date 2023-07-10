a71f8c05ef51bdfcfc9b075cd0387a51109b7f48
7empUser <motorinnikita2406@mail.ru>
2023-07-10 11:37:43 +0300

    Fix bugs with header and groups

--- pages/headerWithReg.php
+++ pages/headerWithReg.php
@@ -4,10 +4,10 @@
             <h1>+RECORD</h1>
         </div>
     </a>
-    <a href="http://ToDoList/pages/profile.php" class="btn firstInHeader">
+    <a href="http://ToDoList/pages/profile.php" class="headerBtn firstInHeader">
         <div><p>Личный кабинет</p></div>
     </a>
-    <a href="http://ToDoList/pages/exit.php" class="btn secondInHeader">
+    <a href="http://ToDoList/pages/exit.php" class="headerBtn secondInHeader">
         <div><p>Выйти</p></div>
     </a>
 </header>
--- scripts/newGroup.php
+++ scripts/newGroup.php
@@ -1,6 +1,6 @@
 <?php
     $_SESSION["countGroups"] += 1;
-    $newGroupSql = "UPDATE `users` SET `CountGroups` = '".$_SESSION["countGroups"]."'";
+    $newGroupSql = "UPDATE `users` SET `CountGroups` = '".$_SESSION["countGroups"]."' WHERE `Login` = '$login'";
     $link->query($newGroupSql);
     $_SESSION["group"] = $_SESSION["countGroups"];
     header("Location: ../pages/profile.php");
