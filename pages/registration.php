501b7c19a3f7d89cb80464e7fb561925f2f719ee
7empUser <motorinnikita2406@mail.ru>
2023-07-17 10:04:41 +0300

    Added responsive web design

--- pages/registration.php
+++ pages/registration.php
@@ -22,25 +22,25 @@
 
         <div class="divRegForm">
             <h2>Регистрация</h2>
-                <form action="registration.php" method="POST">
-                    <label>Логин</br>
-                        <input type="text" name="login" maxlength="25" class="btnInput" required>
-                        <span></span></br>
+            <form action="registration.php" method="POST">
+                <label>Логин</br>
+                    <input type="text" name="login" maxlength="25" class="btnInput" required>
+                    <span></span></br>
+                </label>
+                <label>Email</br>
+                    <input type="email" name="email" maxlength="250" class="btnInput" required>
+                    <span></span></br>
+                </label>
+                <label>Пароль</br>
+                    <input type="password" name="pass" class="btnInput" required>
+                    <span></span></br>
                     </label>
-                    <label>Email</br>
-                        <input type="email" name="email" maxlength="250" class="btnInput" required>
-                        <span></span></br>
-                    </label>
-                    <label>Пароль</br>
-                        <input type="password" name="pass" class="btnInput" required>
-                        <span></span></br>
-                        </label>
-                    <label>Повторите пароль</br>
-                        <input type="password" name="checkPass" class="btnInput" required>
-                        <span></span></br>
-                    </label>
-                    <input type="submit" value="Зарегистрируйтесь" name="reg" class="btnInput">
-                </form>
+                <label>Повторите пароль</br>
+                    <input type="password" name="checkPass" class="btnInput" required>
+                    <span></span></br>
+                </label>
+                <input type="submit" value="Зарегистрируйтесь" name="reg" class="btnInput">
+            </form>
         </div>
 
         <?php
